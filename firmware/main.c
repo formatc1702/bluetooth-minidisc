d0e9abc9c0f232695cfdced1cf6b037c6eaed211
Daniel Rojas <github@danielrojas.net>
2021-05-22 13:28:00 +0200

    Add basic firmware for auto-power-on

---
+++ firmware/main.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED_PIN PB3
+
+int main(void)
+{
+  DDRB |= _BV(LED_PIN); // set LED pin as OUTPUT
+  PORTB = 0b00000000 | _BV(LED_PIN); // set LED pin high
+  _delay_ms(3200); // wait 3.2 sec
+  PORTB = 0b00000000; // set LED pin low
+  DDRB = 0b00000000; // set allpins as INPUT
+  // sleep forever
+}
